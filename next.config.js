9a88ccd7408823f30e8cc279ec9b9da9ed452e94
abhishekkumargupta528 <abhishekkumargupta528@gmail.com>
2022-08-18 21:32:32 +0530

    Final changes

--- next.config.js
+++ next.config.js
@@ -5,11 +5,7 @@ const nextConfig = {
 
 module.exports = nextConfig
 module.exports = {
-   eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
+  
   experimental: {
     forceSwcTransforms: true,
   },
